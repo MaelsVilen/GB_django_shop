1faa1d38b405beef9db74f366a8be24617f2805e
MaelsVilen <maels66@gmail.com>
2020-12-09 19:55:36 +0300

    lesson02 initial

--- mainapp/views.py
+++ mainapp/views.py
@@ -7,3 +7,20 @@ def index(request):
 
 def products(request):
     return render(request, 'mainapp/products.html')
+
+
+def test_context(request):
+    context = {
+        'title': 'добро пожаловать',
+        'username': 'Михаил Пигузов',
+        'products': [
+            {'name': 'Чёрное худи', 'price': '2 990 руб.'},
+            {'name': 'Джинсы', 'price': '5 800 руб.'},
+        ],
+        'promotion': True,
+        'promotion products': [
+            {'name': 'Туфли Dr Martines', 'price': '10 000 руб.'},
+        ],
+    }
+
+    return render(request, 'mainapp/context.html', context)
